b1a8b3f09a40b2da3969f6528833a49d3d0b7c1a
KRANTI SINGH <103580308+krantisweb@users.noreply.github.com>
2023-05-27 10:24:59 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,25 @@
+// //Add Loader and disable clicks on Dropdowns and Confirm Button
+// let czbtn = document.querySelector(".cz-confirm-btn");
+// let czform = document.querySelector(".czForm");
+// let czspin = document.createElement("div");
+// czbtn.onclick = function addczloader () {
+//   czspin.classList.add("czSpinner");
+//   czbtn.innerHTML = "";
+//   czbtn.appendChild(czspin);
+//   czform.setAttribute("disabled","");
+// }
+// //Close Popup
+// const closeIcon = document.querySelector(".cz-modal-close-icon")
+// const modalOverlay = document.querySelector(".cz-popup-overlay")
+// const modal = document.querySelector(".cz-popup");
+
+// // closeIcon.addEventListener("click", e => {
+// //   modal.classList.add('cz-swing-out-top-bck');
+// //   setTimeout(()=>{
+// //     modalOverlay.remove()
+// //   },600)
+  
+// // })
+// modalOverlay.addEventListener("click",()=>{
+//   modal.style.display="none";
+// })
